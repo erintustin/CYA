9c263943f63173d6068f15e8a86f869b721d56c7
Erin Tustin <erintustin@gmail.com>
2022-07-27 22:22:56 -0400

    Added Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import headerIcon1 from '../app/assets/img/headerIcon1.png';
+import headerIcon2 from '../app/assets/img/headerIcon2.png';
+import headerIcon3 from '../app/assets/img/headerIcon3.png';
+import CYAlogo from '../app/assets/img/CYAlogo.png';
+import { Container, Row, Col, Button, Card, CardGroup, Media } from 'reactstrap';
+
+
+
+const Header = () => {
+    return (
+        <Container className='jumbotron jumbotron-top jumbotron-fluid'>
+            <Row>
+                <Col md='6' className='my-auto title text-center order-last order-md-first'>
+                        <h1>Congrats, You're Autistic!</h1>
+                        <h3>a Neurodiversity-Affirming Online Tool Kit</h3>
+                    
+                </Col>
+                <Col>
+                <img className='img img-fluid logo-header' src={CYAlogo} alt='logo'/>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm='4' className='header-card'>
+                    <img src={headerIcon1} alt='headericon1'/>
+                    <h6 className="text-center">
+                         Ending Myths and Stereotypes</h6>
+                     <p className='p-0 text-left'>
+                         Autism has been misrepresented by the medical community,
+                         in the media, and by charitable organizations.</p>
+                </Col>
+                <Col sm='4' className='header-card'>
+                    <img src={headerIcon2} alt='headericon2'/>
+                    <h6 className="text-center">
+                        Nothing About Us Without Us</h6>
+                    <p className="text-left">
+                         Resources written by the #1 experts on the autistic 
+                         experience--autistic people.</p>
+                </Col>
+                <Col sm='4' className='header-card'>
+                    <img src={headerIcon3} alt='headericon3' />
+                    <h6 class="media-header text-center">
+                        Celebrating Neurodiversity</h6>
+                    <p class="text-left">
+                        Autism is a developmental disability--and
+                        disability is a natural part of human diversity.</p>
+                </Col>
+            </Row>
+        </Container>
+    );
+
+};
+
+export default Header;
+
