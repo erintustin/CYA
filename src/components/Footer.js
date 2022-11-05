b5dba6b5ca1631b575273eb8f529e97b0d2874a1
Erin Tustin <erintustin@gmail.com>
2022-11-05 17:32:25 -0400

    Add placeholders for future features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Button, Media } from 'reactstrap';
-import feedbacklogo from '../app/assets/img/feedbacklogo.png';
+import Feedback from './Feedback';
 
 
 const Footer = () => {
@@ -8,34 +8,17 @@ const Footer = () => {
             <Container fluid='true'>
                 <Row>
                     <Col md='8' className='mx-auto my-auto'>
-                        <Media className='row feedback'>
-                            <Media object 
-                                    className='img my-auto mx-auto col-md-4'
-                                    id='feedback-logo' 
-                                    src={feedbacklogo} 
-                                    alt='a colorful brain with a thought bubble coming out of it'/>
-                            
-                            <Media body className=' my-auto mx-auto col-md-8'>
-                                <h3 id='feedback-heading'>Support This Project With Feedback</h3>
-                                <p>Feedback from the autistic community is vital to keeping this toolkit true to its purpose
-                                of compiling the best resources to help newly diagnosed and self-diagnosed Autistic 
-                                people and their loved ones have a neurodiversity-affirming introduction to the Autistic 
-                                neurotype. 
-                                </p>
-                                <div class="text-center">
-                                    <Button href='/feedback/' className="btn btn-light btn-small">Submit Feedback</Button>
-                                </div>
-                            </Media>
-                        </Media>
+                        <Feedback />
                     </Col>
                     <Col className='mx-auto my-auto order-first order-md-last'>
                         <h3>
                             Build Your Own Toolkit
                         </h3>
-                        <p className='mx-auto p-2'>Create a custom toolkit to share with your support system using our curated resources and adding your own favorites from the web.</p>
+                        <h6>Coming Soon!</h6>
+                        <p className='mx-auto p-2'>Use the mobile app to create a custom toolkit to share with your support system using our curated resources and adding your own favorites from the web.</p>
                         <div class="text-center">
-                            <Button className="btn btn-dark btn-small" data-toggle="modal" 
-                            data-target="#comingSoonModal">Get the App</Button>
+                            <Button className="btn btn-dark btn-small" href='https://www.loom.com/share/ffc72ff6967844c9a1f8c5e29746c24a'
+                            data-target="new_window">Watch a Demo</Button>
                         </div>
                     </Col>
                     
---
+++ src/pages/FavoritesPage.js
@@ -0,0 +1,21 @@
+import { Container, Row, Col, Button } from 'reactstrap';
+import ResourcesList from '../features/Resources/ResourcesList';
+
+const ToolkitPage = () => {
+    return (
+        <Container fluid='true' className='confetti'>
+            <Row>
+                <Col xs='4' className='mx-auto mt-5'>
+                    <Button className='btn btn-lg' href='/toolkit'>View All Resources</Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='featured-heading p-5 mx-auto' xs='8' md='6'>
+                    <h1>When this feature becomes available, your favorites will appear here.</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default ToolkitPage;
